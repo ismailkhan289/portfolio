c9ab3680c3fd82f9ee4cac03fdd9a79087d61cd2
ismail khan <ism_kh13@yahoo.com>
2022-07-06 15:09:05 +0500

    adding Eror page and its Styles

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,38 @@
+import { Link } from "react-router-dom";
+import { FaDemocrat } from "react-icons/fa";
+
 
 const Error = () => {
-  return <div>
-    Error compoing 404
-  </div>;
+  return <>
+    <section className="error" style={{
+    display: "flex",
+    backgroundColor:"#00b392",
+    justifyContent:"center",
+    alignItems:"center",
+    flexDirection:"column",
+    height:"100vh"
+
+}}>
+    <h1 style={{color:"white",
+    fontSize:"100px"
+  }}>
+    Error Page not Found 404</h1>
+    
+    < FaDemocrat style={{
+    alignContent:"center",
+    alignItems:'center',
+    justifyContent:'center',
+    fontSize:"100px",
+    color:"white"
+    }} />
+    
+    <Link to="/" style={{backgroundColor:"rgb(69, 151, 116)",
+    padding:10,
+    color:"white",
+    fontSize:"50px"
+  }}>Homepage</Link>
+    </section>
+  </>;
 };
 
 export default Error;
